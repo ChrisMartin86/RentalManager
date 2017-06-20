4c62fa3afbf470db501e50770d9913ac3b7f850b
Christopher Martin <Chris.MartinMS@flhealth.gov>
2017-06-20 12:21:09 -0400

    Add project files.

---
+++ RentalDataWarehouseTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentalDataWarehouseTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
